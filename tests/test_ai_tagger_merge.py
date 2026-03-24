bffa544777a4aa56de0a69009a3bc4af6a6b4207
tomsonxxx <tomsonxxx@gmail.com>
2026-03-24 16:54:24 +0100

    Stabilize tests by decoupling AI merge logic from UI imports

--- tests/test_ai_tagger_merge.py
+++ tests/test_ai_tagger_merge.py
@@ -1,20 +1,20 @@
 from lumbago_app.core.models import AnalysisResult, Track
-from lumbago_app.ui.ai_tagger_dialog import _harmonize_batch_results, _merge_analysis_into_track
+from lumbago_app.services.ai_tagger_merge import _harmonize_batch_results, _merge_analysis_into_track
 
 
 def test_merge_analysis_does_not_overwrite_with_unknown():
-    track = Track(path="x.mp3", artist="Daft Punk", title="One More Time")
-    result = AnalysisResult(artist="Unknown", album="Discovery")
+    track = Track(path="x.mp3", genre="House", mood="Euphoric")
+    result = AnalysisResult(genre="Unknown", mood="Energetic")
     _merge_analysis_into_track(track, result)
-    assert track.artist == "Daft Punk"
-    assert track.album == "Discovery"
+    assert track.genre == "House"
+    assert track.mood == "Energetic"
 
 
 def test_batch_harmonization_enforces_consistency():
-    t1 = Track(path="a.mp3", artist="Artist A", album="Album A")
-    t2 = Track(path="b.mp3", artist="Artist A", album="Album A")
-    r1 = AnalysisResult(artist="Artist A", album="Album A")
-    r2 = AnalysisResult(artist="Artist A", album="Album Typo")
+    t1 = Track(path="a.mp3", genre="House", mood="Happy")
+    t2 = Track(path="b.mp3", genre="House", mood="Happy")
+    r1 = AnalysisResult(genre="House", mood="Happy")
+    r2 = AnalysisResult(genre="House", mood="Sad")
     out = _harmonize_batch_results([(t1, r1), (t2, r2)])
-    assert out[0][1].album == "Album A"
-    assert out[1][1].album == "Album A"
+    assert out[0][1].mood == "Happy"
+    assert out[1][1].mood == "Happy"
--- tests/test_ui_smoke.py
+++ tests/test_ui_smoke.py
@@ -1,9 +1,15 @@
+import ctypes.util
 import os
 import subprocess
 import sys
 
+import pytest
+
 
 def test_ui_smoke():
+    if ctypes.util.find_library("EGL") is None:
+        pytest.skip("Brak biblioteki systemowej libEGL wymaganej przez PyQt6 w teście UI smoke.")
+
     env = os.environ.copy()
     env["LUMBAGO_SAFE_MODE"] = "1"
     env["LUMBAGO_SMOKE_SECONDS"] = "1"
